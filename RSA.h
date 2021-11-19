533215aeb95cde3c9801cd9736116ea1933ab6c2
UnboxTheCat <evanhyd2003@gmail.com>
2021-11-19 17:45:06 -0500

    Added RSA template.

---
+++ RSA.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+
+
+using Key = unsigned long long;
+using ULL = unsigned long long;
+
+namespace RSA
+{
+	
+	extern std::vector<ULL> prime_table;
+	void LoadPrimeTable(const std::string& name);
+
+	ULL Serialize(const std::string& str);
+	std::string Deserialize(ULL number);
+
+
+	std::pair<Key, Key> GenerateKeys();
+	ULL Encrypt(Key e, Key n);
+	ULL Decrypt(Key d, Key n);
+
+
+
+	std::pair<ULL, ULL> EEA(int x, int y);
+}
+
---
+++ Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#define 无 void
+#define 整数 int
+#define 小数 float
+#define 双小数 double
+#define 循环 for
+#define 返回 return
+#define 主程序 main
+#define 使用 using
+#define 名字空间 namespace
+#define 打印 printf
+
+
+使用 名字空间 std;
+
+整数 主程序()
+{
+	整数 a = 10;
+	整数 b = 20;
+
+	循环(整数 i = 0; i < 999; ++i)
+	{
+		a += b;
+	}
+
+	打印("%d\n", a);
+
+	返回 0;
+}
+
+
