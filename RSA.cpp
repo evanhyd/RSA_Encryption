3141f6d30086d83082d24971b9041deb81063f0a
UnboxTheCat <evanhyd2003@gmail.com>
2021-11-19 19:08:37 -0500

    Updated RSA template

--- RSA.cpp
+++ RSA.cpp
@@ -1,3 +1,59 @@
 ﻿#include "RSA.h"
+#include <iostream>
+#include <fstream>
+#include <random>
+
+using namespace std;
+
+std::vector<ULL> RSA::prime_table;
+
+bool RSA::LoadPrimeTable(const std::string& name)
+{
+	ifstream input_file(name);
+	if (!input_file.is_open()) return false;
+
+	ULL prime_num;
+	while (input_file >> prime_num)
+	{
+		RSA::prime_table.push_back(prime_num);
+	}
+
+	return true;
+}
 
 
+
+std::pair<Key, Key> RSA::GenerateKeys()
+{
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<unsigned int> gen_prime(0, RSA::prime_table.size() - 1);
+
+	ULL p = RSA::prime_table[gen_prime(rng)];
+	ULL q = RSA::prime_table[gen_prime(rng)];
+	ULL n = p * q;
+	ULL n_l = (p - 1) * (q - 1);
+
+
+	ULL e = 1;
+	for (; e < n_l; ++e)
+	{
+
+	}
+
+
+	
+
+
+}
+
+
+ULL Encrypt(Key e, Key n);
+ULL Decrypt(Key d, Key n);
+
+
+
+std::pair<ULL, ULL> RSA::EEA(ULL x, ULL y)
+{
+
+}
--- RSA.h
+++ RSA.h
@@ -10,18 +10,15 @@ namespace RSA
 {
 	
 	extern std::vector<ULL> prime_table;
-	void LoadPrimeTable(const std::string& name);
-
-	ULL Serialize(const std::string& str);
-	std::string Deserialize(ULL number);
-
+	bool LoadPrimeTable(const std::string& name);
+	
+	std::pair<ULL, ULL> RSA::EEA(ULL x, ULL y);
+	std::vector<ULL> Serialize(const std::string& str);
+	std::string Deserialize(const std::vector<ULL>& serialized_data);
 
 	std::pair<Key, Key> GenerateKeys();
-	ULL Encrypt(Key e, Key n);
-	ULL Decrypt(Key d, Key n);
+	ULL Encrypt(ULL plain, Key e, Key n);
+	ULL Decrypt(ULL cypher, Key d, Key n);
 
-
-
-	std::pair<ULL, ULL> EEA(int x, int y);
 }
 
